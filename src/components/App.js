7659874166f1798a204e2fb7f6dcb6f5f51fc159
daffriedman <dafriedman443@gmail.com>
2021-06-29 21:11:19 +0300

    Using context in a slanguage selector

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+class App extends React.Component {
+    state={language:'english'}
+    onLanguageChange = language => {
+        this.setState({language})
+    }
+    render(){
+
+        return(
+            <div className="ui container">
+            <div>
+                Select a language
+                <i className="flag us" onClick={()=> this.onLanguageChange('english')}/>
+                <i className="flag il" onClick={()=> this.onLanguageChange('hebrew')}/>
+            </div>
+            <ColorContext.Provider value='yellow'>
+
+            <LanguageContext.Provider value={this.state.language}>
+
+           <UserCreate/>
+            </LanguageContext.Provider>
+            </ColorContext.Provider>
+        </div>
+    )
+}
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+class Button extends React.Component{
+    //Only needed for a context type
+//   static contextType = LanguageContext;
+  
+    render(){
+         //Only needed for a context type
+        // const text = this.context === 'english'? 'Submit': 'שלח';
+        return(
+            <ColorContext.Consumer>
+            {
+                (color)=>
+
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {(value)=> value==='english'? 'Submit':'שלח' }
+                </LanguageContext.Consumer>
+            </button>
+            }
+
+            </ColorContext.Consumer>
+        )
+        
+    }
+}
+
+export default Button;
